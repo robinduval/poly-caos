6d8402a0dc77e50a6485f0a3da8df833a208e5a4
robinduval <leyoda@gmail.com>
2023-04-08 22:05:27 +0200

    Exercice 8

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -253,7 +253,7 @@ int func7(int x, int n) {
  *   Rating: 1
  */
 int func8(void) {
-    return ~0 << 31; // Shift the bitwise negation of 0 to the left by 31 positions
+    return ~0 << 31;                              // Shift the bitwise negation of 0 to the left by 31 positions
 }
 /*
  * func9 - returns 1 if x is the maximum, two's complement number,
