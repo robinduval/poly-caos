069c052a199b4cd0c50014b8bcb475ad6482cb34
robinduval <leyoda@gmail.com>
2023-04-10 22:43:50 +0200

    Exercice 4 : fix negative value

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -191,14 +191,13 @@ int func3(int x, int y) {
  *   Rating: 2
  */
 int func4(int x) {
-  int mask_high = ((0xFF << 8) | 0xFF) << 16; // 0xFFFF0000
-  int mask_low = ~mask_high;                 // 0x0000FFFF
-  int first16b = x & mask_high;
-  int last16b = x & mask_low;
-  int shifted_first16b = (first16b >> 16) & ((1 << 15) + 0xFF); // Clear sign bit
-  int sign_bit = first16b & (1 << 15); // Extract the sign bit after shifting
-  
-  return (last16b << 16) | shifted_first16b | (sign_bit << 1);
+    int mask_high = ((0xFF << 8) | 0xFF) << 16; // 0xFFFF0000
+    int mask_low = ~mask_high;                 // 0x0000FFFF
+    int first16b = x & mask_high;
+    int last16b = x & mask_low;
+    int shifted_first16b = (first16b >> 16) | (first16b >> 15 & 0x100); // Preserve sign bit
+    
+    return (last16b << 16) | shifted_first16b;
 
   // int first16b = (x >> 16) & 0xFFFF; // Extract the first 16 bits of x by right-shifting x by 16 and masking with 0xFFFF.
   // int last16b = x & 0xFFFF;          // Extract the last 16 bits of x by simply masking x with 0xFFFF.
