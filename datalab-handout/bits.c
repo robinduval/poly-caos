b46f16baa7a5c5d06f44b1e104c765628ca8e7f6
robinduval <leyoda@gmail.com>
2023-04-10 23:01:21 +0200

    Exercice 4 Try to find a solution

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -191,13 +191,14 @@ int func3(int x, int y) {
  *   Rating: 2
  */
 int func4(int x) {
-    int mask_high = (((0xFF << 8) | 0xFF) << 8) << 8; // 0xFFFF0000
-    int mask_low = ~mask_high;                       // 0x0000FFFF
+    int mask_high = ((0xFF << 8) | 0xFF) << 16; // 0xFFFF0000
+    int mask_low = ~mask_high;                 // 0x0000FFFF
     int first16b = x & mask_high;
     int last16b = x & mask_low;
-    int shifted_first16b = (first16b >> 16) | (first16b >> 15 & (1 << 7));
+    int shifted_first16b = (first16b >> 16) & 0x7FFF; // Clear sign bit
+    int sign_bit = first16b >> 31;                   // Extract the sign bit
     
-    return (last16b << 16) | shifted_first16b;
+    return (last16b << 16) | shifted_first16b | (sign_bit << 15);
 
   // int first16b = (x >> 16) & 0xFFFF; // Extract the first 16 bits of x by right-shifting x by 16 and masking with 0xFFFF.
   // int last16b = x & 0xFFFF;          // Extract the last 16 bits of x by simply masking x with 0xFFFF.
